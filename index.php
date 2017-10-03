680edd4f5b3119b4f65c9c7e166aa5f7d187d6d6
Sanket Nandakumar Pednekar <sp2363@afsconnect2.njit.edu>
2017-10-03 02:45:09 -0400

    string word count

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 			$year = array("2012", "396", "300","2000", "1100", "1089");
 			echo "The value of \$year: ";
 			print_r($year);
+
 			$this->html.=htmlTags::horizontalRule();
 		
 			$this->html .= htmlTags::headingTwo("String Replace Demo");
@@ -21,27 +22,34 @@
 			$this->html .= htmlTags::headingTwo("String Comparison Demo");
 			$this->html .= strings::stringCompare($date,$tar);
 			$this->html .= htmlTags::horizontalRule();
-									
+			
+			$this->html .= htmlTags::headingTwo("String Word Count Demo");
+			$this->html .= strings::stringWordcount($date);
+			$this->html .= htmlTags::horizontalRule();
+
 		}
 		public function __destruct() {
 			print($this->html);
 		}
 	}
 
-	class strings {
-		static public function stringReplace($src,$target,$str){
-			return str_replace($src,$target,$str);
-		}
-		static public function stringCompare($string1,$string2){
-			if(strcmp($string1,$string2)==0){
-				return "Oops";
-			}else if(strcmp($string1,$string2)<0) {
-				return "Future";
-			}else {
-				return "Past";
+		class strings {
+  			static public function stringReplace($src,$target,$str){
+		  		return str_replace($src,$target,$str);
+				}
+			static public function stringCompare($string1,$string2){
+				if(strcmp($string1,$string2)==0){
+					return "Oops";
+				}else if(strcmp($string1,$string2)<0) {
+					return "Future";
+				}else {
+					return "Past";
+				}
 			}
+			static public function stringWordcount($string1){
+				 	return str_word_count($string1);
+				}
 		}
-	}
 	class htmlTags
 	{
 		static public function horizontalRule() {
