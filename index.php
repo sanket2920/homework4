573d18693dfa57f61a4911335f357cc2393f7438
Sanket Nandakumar Pednekar <sp2363@afsconnect2.njit.edu>
2017-10-03 01:46:14 -0400

    string replace

--- index.php
+++ index.php
@@ -13,9 +13,19 @@
 			echo "The value of \$year: ";
 			print_r($year);
 			$this->html.=htmlTags::horizontalRule();
+		
+			$this->html .= htmlTags::headingTwo("String Replace Demo");
+			$this->html .= strings::stringReplace('-','/',$date);
+			$this->html .= htmlTags::horizontalRule();
 		}
 		public function __destruct() {
 			print($this->html);
+		}
+	}
+
+	class strings {
+		static public function stringReplace($src,$target,$str){
+			return str_replace($src,$target,$str);
 		}
 	}
 	class htmlTags
@@ -23,11 +33,11 @@
 		static public function horizontalRule() {
 	          	return '<hr>';
 		}
-		static public function headingOne($text) {
-			return '<h1>' . $text . '</h1>';
+		static public function headingTwo($text) {
+			return '<h2>' . $text . '</h2>';
 		}
-		static public function headingThree($text) {
-			return '<h3>' .$text . '</h1>';
+		static public function headingFour($text) {
+			return '<h4>' .$text . '</h4>';
 		}
 	}		
 ?>
